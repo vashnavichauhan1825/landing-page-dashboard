0a371df2994b6f66f82f909a36fef85d1803f849
vashnavichauhan1825 <vashnavichauhan1@gmail.com>
2024-06-02 01:13:15 +0530

    style: create form

--- pages/landingpagecreate.js
+++ pages/landingpagecreate.js
@@ -4,9 +4,7 @@ const LandingPageCreate = () => {
   const [formData, setFormData] = useState({
     title: "",
     description: "",
-    headerContent: "",
     textContent: "",
-    footerContent: "",
   });
 
   const handleChange = (e) => {
@@ -21,63 +19,60 @@ const LandingPageCreate = () => {
   };
 
   return (
-    <div>
-      <h1>Create a New Landing Page</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Title:</label>
+    <div className="flex flex-col items-center gap-10">
+      <h1 className="text-3xl font-bold mt-6">Create a New Landing Page</h1>
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-md mx-auto  w-[40%] px-6 bg-[var(--primary-color)] rounded-md py-8 grid grid-cols-1 gap-8"
+      >
+        <div className="flex flex-col">
+          <label
+            htmlFor="title"
+            className="text-[var(--secondary-color)] opacity-90 text-lg"
+          >
+            Title:
+          </label>
           <input
             type="text"
             id="title"
             name="title"
             value={formData.title}
             onChange={handleChange}
+            className="border border-[var(--ter-color)] rounded-md p-2 w-full"
             required
           />
         </div>
+
         <div>
           <label htmlFor="description">Description:</label>
-          <textarea
+          <input
+            type="text"
             id="description"
             name="description"
-            rows="4"
             value={formData.description}
-            onChange={handleChange}
-            required
-          ></textarea>
-        </div>
-        <div>
-          <label htmlFor="headerContent">Header Content:</label>
-          <input
-            type="text"
-            id="headerContent"
-            name="headerContent"
-            value={formData.headerContent}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="textContent">Text Content:</label>
-          <input
-            type="text"
-            id="textContent"
-            name="textContent"
-            value={formData.textContent}
+            className="border border-[var(--ter-color)] rounded-md p-2 w-full"
             onChange={handleChange}
           />
         </div>
 
         <div>
-          <label htmlFor="footerContent">Footer Content:</label>
-          <input
-            type="text"
-            id="footerContent"
-            name="footerContent"
-            value={formData.footerContent}
+          <label htmlFor="textContent">About Text:</label>
+          <textarea
+            id="textContent"
+            name="textContent"
+            rows="4"
+            value={formData.textContent}
+            className="border border-[var(--ter-color)] rounded-md p-2 w-full"
             onChange={handleChange}
-          />
+            required
+          ></textarea>
         </div>
-        <button type="submit">Create Landing Page</button>
+        <button
+          type="submit"
+          className="mx-auto w-full bg-[var(--cta-color)] rounded-md my-4 p-2 hover:opacity-90 font-semibold hover:text-[var(--primary-color)]"
+        >
+          Create Landing Page
+        </button>
       </form>
     </div>
   );

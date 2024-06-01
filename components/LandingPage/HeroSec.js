1de0f518aec49a2f5cca8fe29ca8b7a791663ae0
vashnavichauhan1825 <vashnavichauhan1@gmail.com>
2024-06-02 00:13:39 +0530

    feat:Hero section

---
+++ components/LandingPage/HeroSec.js
@@ -0,0 +1,22 @@
+import Image from "./Image";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+const HeroSec = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const landingPages = useSelector((state) => state.landingPages.landingPages);
+  console.log(id);
+  const landingPage = landingPages.find((page) => page.id === id.toString());
+  console.log(landingPage);
+  return (
+    <div className="w-full flex justify-center gap-11 items-center h-[60vh] pt-10 px-8">
+      <div>
+        <h1 className="text-5xl font-bold">{landingPage.title}</h1>
+        <p className="text-3xl opacity-50 mt-6">{landingPage.description}</p>
+      </div>
+      <Image />
+    </div>
+  );
+};
+
+export default HeroSec;
--- pages/landingpage/[id].js
+++ pages/landingpage/[id].js
@@ -1,22 +1,14 @@
 import Header from "@/components/LandingPage/Header";
 import Footer from "@/components/LandingPage/Footer";
-import { useRouter } from "next/router";
 import TextBlock from "@/components/LandingPage/TextBlock";
 import Image from "@/components/LandingPage/Image";
+import HeroSec from "@/components/LandingPage/HeroSec";
 
 export default function LandingPage() {
-  const router = useRouter();
-  const { id } = router.query;
-
   return (
     <div>
       <Header />
-      <div className="h-[80vh]">
-        <h1>Landing Page Details</h1>
-        <p>Landing Page ID: {id}</p>
-        <Image />
-      </div>
-
+      <HeroSec />
       <TextBlock />
       <Footer />
     </div>

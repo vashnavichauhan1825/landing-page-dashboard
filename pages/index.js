c477e1c38212f270386acec87775e8ccde44a903
vashnavichauhan1825 <vashnavichauhan1@gmail.com>
2024-06-01 19:00:24 +0530

    data: add mock api with db.json

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 const HomePage = () => {
   return (
-    <div>
-      <h1>Landing Page Dashboard</h1>
+    <div className="p-4">
+      <h1 className="text-4xl font-bold mt-3">Landing Page Dashboard</h1>
+      <ul>
+        {<li>item</li>}
+        <li></li>
+      </ul>
     </div>
   );
 };

fb0f123c30675dea0cc58e9311d2b95d1944dec0
vashnavichauhan1825 <vashnavichauhan1@gmail.com>
2024-06-01 12:55:17 +0530

    feat: auth route

---
+++ pages/auth.js
@@ -0,0 +1,12 @@
+import AuthForm from "@/components/Auth/AuthForm";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <AuthForm />
+    </div>
+  );
+};
+
+export default LoginPage;

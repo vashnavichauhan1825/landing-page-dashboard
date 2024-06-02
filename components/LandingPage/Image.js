4d7be27a9c7024843199a67613ef279e6855992f
vashnavichauhan1825 <vashnavichauhan1@gmail.com>
2024-06-02 08:23:06 +0530

    fix: handle image

--- components/LandingPage/Image.js
+++ components/LandingPage/Image.js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 const Image = () => {
@@ -8,16 +9,20 @@ const Image = () => {
   console.log(id);
   const landingPage = landingPages.find((page) => page.id === id.toString());
   console.log(landingPage);
+  const [imgSrc, setImgSrc] = useState(landingPage?.components[2].src || "");
+
+  const handleImageError = () => {
+    setImgSrc(
+      "https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?q=80&w=2946&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    );
+  };
   return (
     <div className=" w-96 h-96 rounded-[50%] overflow-hidden shadow-lg">
       <img
         className="object-cover w-full h-full"
         alt="landing page image"
-        src={
-          landingPage.components[2].src
-            ? landingPage.components[2].src
-            : `https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?q=80&w=2946&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`
-        }
+        src={imgSrc}
+        onError={handleImageError}
       />
     </div>
   );
